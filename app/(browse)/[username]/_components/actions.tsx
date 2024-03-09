1d7b2068cede268016e47c470e6d71ba6348bea0
Sam <samarooniii@yahoo.com>
2024-03-09 14:46:58 -0500

    added blocking/unblocking functionality

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { onBlock } from '@/actions/block';
 import { onFollow, onUnfollow } from '@/actions/follow';
 import { Button } from '@/components/ui/button';
 import { useTransition } from 'react';
@@ -41,9 +42,24 @@ export const Actions = ({ isFollowing, userId }: ActionProps) => {
     }
   };
 
+  const handleBlock = () => {
+    startTransition(() => {
+      onBlock(userId)
+        .then((data) =>
+          toast.success(`Blocked the user ${data.blocked.username}`)
+        )
+        .catch(() => toast.error('Something went wrong...'));
+    });
+  };
+
   return (
-    <Button disabled={isPending} onClick={onClick} variant="primary">
-      {isFollowing ? 'Unfollow' : 'Follow'}
-    </Button>
+    <>
+      <Button disabled={isPending} onClick={onClick} variant="primary">
+        {isFollowing ? 'Unfollow' : 'Follow'}
+      </Button>
+      <Button onClick={handleBlock} disabled={isPending}>
+        Block
+      </Button>
+    </>
   );
 };
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { useEffect, useState } from 'react';
 import { ToggleSkeleton } from './toggle';
 import { RecommendedSkeleton } from './recommended';
+import { FollowingSkeleton } from './following';
 
 interface WrapperProps {
   children: React.ReactNode;
@@ -22,6 +23,7 @@ export const Wrapper = ({ children }: WrapperProps) => {
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
         <ToggleSkeleton />
+        <FollowingSkeleton />
         <RecommendedSkeleton />
       </aside>
     );
